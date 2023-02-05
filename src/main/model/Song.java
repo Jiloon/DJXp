76c7f8d6728b8c4bc4f04bfa56dd348094336840
jiloon <jiloon@students.cs.ubc.ca>
2023-02-05 00:06:19 -0800

    - Added a few required classes - Added in fields into Song class - Added setup in SongTest class

---
+++ src/main/model/Song.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Song {
+    private int length;
+    private int bpm;
+    private String key;
+    private String name;
+    private ArrayList<String> artists;
+    private String fileAddress;
+    private double speed;
+    private double volume;
+
+    public Song() {
+
+    }
+}
---
+++ src/test/model/SongTest.java
@@ -0,0 +1,13 @@
+package model;
+
+import model.Song;
+import org.junit.jupiter.api.BeforeEach;
+
+class SongTest {
+    private Song song;
+
+    @BeforeEach
+    void setup() {
+
+    }
+}
