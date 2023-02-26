6464a9fdd428c0a3c31976618f24a37d8e206dd1
jiloon <jiloon@students.cs.ubc.ca>
2023-02-26 15:33:25 -0800

    - Fixed libraries (for real this time)

--- src/test/model/SetTest.java
+++ src/test/model/SetTest.java
@@ -1,12 +1,17 @@
 package model;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.ArrayList;
 
 // Test class for Set
 public class SetTest {
-    private Set set;
+    private Set set0;
     private Set set1;
     private Set set2;
     private Set set3;
@@ -23,9 +28,37 @@ public class SetTest {
         song2 = new Song("Judah and the Lion - Why did you run (JILOON Remix)");
         songList.add(song0);
         songList.add(song1);
-        set = new Set("MySet");
+        set0 = new Set("MySet");
         set1 = new Set("MySet", "EDM");
         set2 = new Set("MySet", "EDM", songList);
         set3 = new Set();
+    }
+
+    @Test
+    void testConstructorNoParams() {
+        assertTrue(set3.getSongs().isEmpty());
+        assertEquals("", set3.getName());
+        assertEquals("", set3.getGenre());
+    }
+
+    @Test
+    void testConstructorName() {
+        assertTrue(set0.getSongs().isEmpty());
+        assertEquals("MySet", set3.getName());
+        assertEquals("", set3.getGenre());
+    }
+
+    @Test
+    void testConstructorNameGenre() {
+        assertTrue(set1.getSongs().isEmpty());
+        assertEquals("MySet", set3.getName());
+        assertEquals("EDM", set3.getGenre());
+    }
+
+    @Test
+    void testConstructorNameGenreSongs() {
+        assertEquals(set2.getSongs(), songList);
+        assertEquals("MySet", set3.getName());
+        assertEquals("EDM", set3.getGenre());
     }
 }
