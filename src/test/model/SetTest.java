afbc505ce8284d28fe602275ba43059e6e027085
jiloon <jiloon@students.cs.ubc.ca>
2023-02-26 16:04:07 -0800

    - Reverted the mess

--- src/test/model/SetTest.java
+++ src/test/model/SetTest.java
@@ -22,43 +22,20 @@ public class SetTest {
 
     @BeforeEach
     void setup() {
-        songList = new ArrayList<>();
         song0 = new Song("Martin Garrix, Dean Lewis - Used To Love");
         song1 = new Song("Kygo x NOTD - Think About You x Nobody (JILOON Remix)");
         song2 = new Song("Judah and the Lion - Why did you run (JILOON Remix)");
+        songList = new ArrayList<>();
         songList.add(song0);
         songList.add(song1);
-        set0 = new Set("MySet");
-        set1 = new Set("MySet", "EDM");
-        set2 = new Set("MySet", "EDM", songList);
-        set3 = new Set();
+        set0 = new Set();
+        set1 = new Set("MySet");
+        set2 = new Set("MySet", "EDM");
+        set3 = new Set("MySet", "EDM", songList);
     }
 
     @Test
     void testConstructorNoParams() {
-        assertTrue(set3.getSongs().isEmpty());
-        assertEquals("", set3.getName());
-        assertEquals("", set3.getGenre());
-    }
-
-    @Test
-    void testConstructorName() {
-        assertTrue(set0.getSongs().isEmpty());
-        assertEquals("MySet", set3.getName());
-        assertEquals("", set3.getGenre());
-    }
-
-    @Test
-    void testConstructorNameGenre() {
-        assertTrue(set1.getSongs().isEmpty());
-        assertEquals("MySet", set3.getName());
-        assertEquals("EDM", set3.getGenre());
-    }
-
-    @Test
-    void testConstructorNameGenreSongs() {
-        assertEquals(set2.getSongs(), songList);
-        assertEquals("MySet", set3.getName());
-        assertEquals("EDM", set3.getGenre());
+        assertTrue();
     }
 }
