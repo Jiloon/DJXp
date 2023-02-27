1c81831d0db26a475e828e22ad192898ee5b3444
jiloon <jiloon@students.cs.ubc.ca>
2023-02-26 16:04:41 -0800

    Revert "- Reverted the mess"
    This reverts commit 6f1b24294aa214cc727cebd11f9071f3d553a890.

--- src/test/model/SetTest.java
+++ src/test/model/SetTest.java
@@ -1,41 +1,13 @@
 package model;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
-import java.util.ArrayList;
 
 // Test class for Set
 public class SetTest {
-    private Set set0;
-    private Set set1;
-    private Set set2;
-    private Set set3;
-    private Song song0;
-    private Song song1;
-    private Song song2;
-    private ArrayList<Song> songList;
+    private Set set;
 
     @BeforeEach
     void setup() {
-        song0 = new Song("Martin Garrix, Dean Lewis - Used To Love");
-        song1 = new Song("Kygo x NOTD - Think About You x Nobody (JILOON Remix)");
-        song2 = new Song("Judah and the Lion - Why did you run (JILOON Remix)");
-        songList = new ArrayList<>();
-        songList.add(song0);
-        songList.add(song1);
-        set0 = new Set();
-        set1 = new Set("MySet");
-        set2 = new Set("MySet", "EDM");
-        set3 = new Set("MySet", "EDM", songList);
-    }
 
-    @Test
-    void testConstructorNoParams() {
-        assertTrue();
     }
 }
